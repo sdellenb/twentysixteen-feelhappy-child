4f54e3b9acf871c2af996beac628ed6c743b7020
Simon Dellenbach <sdellenb@gmail.com>
2015-12-12 21:40:25 +0100

    Extend footer, add license file and bump version to 1.0.
    Adding the link to the theme's GitHub page and added the (unmodified)
    GPLv2 license file, the markdown variant from
    https://github.com/nevir/readable-licenses.
    
    Because I'm copying and modifying the original Twenty Sixteen theme
    files, the Twenty Sixteen Feelhappy Child theme is also licended under
    the GNU General Public License v2.0.

--- footer.php
+++ footer.php
@@ -39,16 +39,22 @@
 			<?php endif; ?>
 
 			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+				<ul>
+					<li>
+						<?php
+							/**
+							 * Fires before the twentysixteen footer text for footer customization.
+							 *
+							 * @since Twenty Sixteen 1.0
+							 */
+							do_action( 'twentysixteen_credits' );
+						?>
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+					</li>
+					<li>
+						<a href="https://github.com/sdellenb/twentysixteen-feelhappy-child">Twenty Sixteen Feelhappy Child Theme</a> &copy;2015 Simon Dellenbach
+					</li>
+				</ul>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
