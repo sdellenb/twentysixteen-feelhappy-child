a7592c5c72fc88989391820dda02e96495c51743
Simon Dellenbach <simon.dellenbach@autodesk.com>
2018-05-22 13:35:46 +0200

    Fix Privacy Policy link in footer.

--- footer.php
+++ footer.php
@@ -63,7 +63,7 @@
 					</li>
 					<?php
 						if ( function_exists( 'the_privacy_policy_link' ) ) {
-							the_privacy_policy_link( '', '<li><span role="separator" aria-hidden="true"></span></li>' );
+							the_privacy_policy_link( '<li>', '</li>' );
 						}
 					?>
 				</ul>
